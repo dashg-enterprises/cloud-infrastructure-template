8ed7cf0a4b20628a66476f199d0b5107b01eefad
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-27 23:08:37 -0700

    Add optional flags to cloudscape

--- iac/aws/cloudscape/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  organization_name = "Put your git org name here"
-  domain_name = "Put your domain here. example: mysite.com"
-  bounded_contexts = []
-}
--- iac/aws/cloudscape/main.tf
+++ iac/aws/cloudscape/main.tf
@@ -9,6 +9,12 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+locals {
+  organization_name = "Put your git org name here"
+  domain_name = "Put your domain here. example: mysite.com"
+  bounded_contexts = []
+}
+
 module "environment_on_aws" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/cloudscape/aws?ref=main"
   organization_name = local.organization_name
@@ -16,4 +22,7 @@ module "environment_on_aws" {
   bounded_contexts = local.bounded_contexts
   environment_name = var.environment_name
   environment_subdomain = var.environment_subdomain // e.g. 'www' for prod
+  with_eks = true
+  with_api_gw = true
+  with_nat_gw = true
 }
